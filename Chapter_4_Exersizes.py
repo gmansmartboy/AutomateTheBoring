9ee693a31bac41bdd34bf7eec18416cad2cd2046
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2022-04-08 19:21:26 -0700

    Update and rename Chapter 1 Exersizes to Chapter_4_Exersizes.py

---
+++ Chapter_4_Exersizes.py
@@ -0,0 +1,16 @@
+# Lists in Python
+# Lists with ints. 
+spam = [1, 2, 3]
+print('spam = ' + str(spam))
+
+# Lists with 
+eggs = ['cat', 'bat', 'rat', 'elephant']
+print('eggs = ' + str(eggs))
+
+print(spam)
+print(eggs)
+
+# Getting individual values in a list with indexes
+
+print(eggs[0])
+print('The ' + eggs[0] + ' ate the ' + eggs[1] + '.')
