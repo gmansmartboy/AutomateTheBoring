b957357a339b6229f67673e46cda1050f58dacb3
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2022-04-08 21:59:51 -0700

    Update Chapter_4_Exersizes.py

--- Chapter_4_Exersizes.py
+++ Chapter_4_Exersizes.py
@@ -34,3 +34,25 @@ print(spam[-3]) # Prints 'bat'
 
 print('The ' + spam[-1] + ' is afraid of the ' + spam[-3] + '.')
 # Prints 'The elephant is afraid of the bat.'
+
+# A slice can get several values from a list,
+# in the form of a new list. 
+spam = ['cat', 'bat', 'rat', 'elephant']
+print('spam = ' + str(spam))
+print(spam[1:3]) # Prints ['bat', 'rat']
+print(spam[0:-1]) # Prints ['cat', 'bat', 'rat']
+
+# Leaving out the first index is the same as using 0,
+# or the beginning of the list.
+# Leaving out the second index is the same as using
+# the length of the list.
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[:2]) # Prints  ['cat', 'bat']
+print (spam[1:]) # Prints ['bat', 'rat', 'elephant']
+print(spam[:]) # Prints ['cat', 'bat', 'rat', 'elephant']
+
+# The len() function will return the number of
+# values that are in a list value passed to it.
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(len(spam)) # Prints 3
