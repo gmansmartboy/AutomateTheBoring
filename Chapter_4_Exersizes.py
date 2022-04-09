30557ba3f497e140ef78b9b46601522e8ffd2808
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2022-04-08 21:29:40 -0700

    Update Chapter_4_Exersizes.py

--- Chapter_4_Exersizes.py
+++ Chapter_4_Exersizes.py
@@ -18,3 +18,19 @@ print('The ' + eggs[0] + ' ate the ' + eggs[1] + '.')
 
 # The following will not produce a type error:
 print(spam[int(1.0)])
+
+# List can contain other lists
+spam = [['cat', 'bat'], [10, 20, 30, 40, 50]]
+print('spam =  ' + str(spam))
+print(spam[0])  # Prints ['cat', 'bat']
+print(spam[0][1])  #Prints 'bat'
+print(spam[1][4])  # Prints 50
+
+# The integer value -1 refers to the last index in a list
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam)
+print(spam[-1]) # Prints 'elephant'
+print(spam[-3]) # Prints 'bat'
+
+print('The ' + spam[-1] + ' is afraid of the ' + spam[-3] + '.')
+# Prints 'The elephant is afraid of the bat.'
