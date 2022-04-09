0a8ea1b6de5da95205efddff79bf10cea61d9aeb
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2022-04-08 19:29:49 -0700

    Update Chapter_4_Exersizes.py

--- Chapter_4_Exersizes.py
+++ Chapter_4_Exersizes.py
@@ -3,14 +3,18 @@
 spam = [1, 2, 3]
 print('spam = ' + str(spam))
 
-# Lists with 
+# Lists with string
 eggs = ['cat', 'bat', 'rat', 'elephant']
 print('eggs = ' + str(eggs))
 
-print(spam)
-print(eggs)
-
 # Getting individual values in a list with indexes
-
 print(eggs[0])
 print('The ' + eggs[0] + ' ate the ' + eggs[1] + '.')
+
+# The following will produce an index error:
+#print(spam[1000])
+# The following will produce a type error:
+#spam[1.0]
+
+# The following will not produce a type error:
+print(spam[int(1.0)])
