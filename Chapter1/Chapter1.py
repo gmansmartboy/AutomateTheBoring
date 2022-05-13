1ef5e0925217005720e7b6c779a77d7c1ff15f36
gmansmartboy <88818390+gmansmartboy@users.noreply.github.com>
2022-05-12 17:31:20 -0700

    Chaper 1 addition

---
+++ Chapter1/Chapter1.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for my name.
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It \'s nice to meet you ' + str(myName))
+print('Your name has ' + str(len(myName)) + ' leters in it.')
+
